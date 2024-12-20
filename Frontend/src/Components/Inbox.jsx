d3539b01610882ba254ea93be12438f28ed7a628
Patel Het <125752460+hetpatel2508@users.noreply.github.com>
2024-12-20 10:43:28 +0530

    .

--- Frontend/src/Components/Inbox.jsx
+++ Frontend/src/Components/Inbox.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
+import {
+  FaAngleDown,
+  FaArrowRotateRight,
+  FaEllipsisVertical,
+  FaChevronLeft,
+  FaChevronRight,
+} from 'react-icons/fa6';
 
 export default function Inbox() {
   return (
-    <div
-      className="bg-gray-500 w-[calc(100vw-75.5px)] h-full flex items-center text-2xl 
-    font-bold text-white ml-[76px]"
-    >
-      Inbox
+    <div className="bg-red-200 w-[calc(100vw-75.5px)] h-full flex flex-col ml-[76px]">
+      <div className="w-full h-[48px] bg-gray-300 flex items-center justify-between">
+        <div className="h-full w-[150px] bg-red-100 flex items-center gap-1 ml-2.5">
+          <div className="h-[36px] w-[calc(100%-90px)] bg-cyan-400 flex items-center ">
+            <div className="w-[26px] h-[26px] flex items-center pl-2">
+              <input type="checkbox" className="w-[20px] h-[20px]" />
+            </div>
+            <div className="pl-2">
+              <FaAngleDown size={14} />
+            </div>
+          </div>
+          <div className="h-[40px] w-[40px] bg-cyan-100 flex justify-center items-center cursor-pointer rounded-full hover:bg-gray-200">
+            <FaArrowRotateRight size={16} />{' '}
+          </div>
+          <div className="h-[40px] w-[40px] bg-cyan-100 flex justify-center items-center cursor-pointer rounded-full hover:bg-gray-200">
+            <FaEllipsisVertical size={16} />{' '}
+          </div>
+        </div>
+        <div className="h-full w-[190px] bg-red-100 flex items-center gap-1">
+          <div className="h-full w-[calc(100%-90px)] bg-cyan-400 flex items-center gap-1 justify-center">
+            <div>{`1`}</div>
+            <div>to</div>
+            <div>{`50`}</div>
+          </div>
+          <div className="h-[40px] w-[40px] bg-cyan-100 flex justify-center items-center cursor-pointer rounded-full hover:bg-gray-200">
+            <FaChevronLeft />
+          </div>
+          <div className="h-[40px] w-[40px] bg-cyan-100 flex justify-center items-center cursor-pointer rounded-full hover:bg-gray-200">
+            <FaChevronRight />
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[56px] flex bg-green-100">
+        <div className="h-full w-[256px] flex items-center bg-gray-100">
+          <div className='w-[56px] h-full bg-cyan-100'></div>
+        </div>
+        <div className="h-full w-[256px] flex items-center bg-blue-100">
+          <div className='w-[56px] h-full bg-cyan-100'></div>
+        </div>
+        <div className="h-full w-[256px] flex items-center bg-red-100">
+          <div className='w-[56px] h-full bg-cyan-100'></div>
+        </div>
+        <div className="h-full w-[256px] flex items-center bg-pink-100">
+          <div className='w-[56px] h-full bg-cyan-100'></div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -21,7 +21,7 @@ export default function Sidebar() {
 
     refArray.current.forEach((element, index) => {
       if (index === currentPage) {
-        element.classList.add('bg-cyan-200');
+        element.classList.add('bg-[#D3E3FD]');
       }
       element.classList.remove('max-w-[53px]');
       element.classList.add('max-w-[240px]');
@@ -32,7 +32,7 @@ export default function Sidebar() {
     ref1.current.style.maxWidth = '56px';
 
     refArray.current.forEach((element, index) => {
-      element.classList.remove('bg-cyan-200');
+      element.classList.remove('bg-[#D3E3FD]');
       element.classList.remove('max-w-[240px]');
       element.classList.add('max-w-[53px]');
     });
@@ -40,12 +40,12 @@ export default function Sidebar() {
 
   return (
     <div
-      className="bg-red-100  w-[75.5px] h-[calc(100vh-61px)] flex flex-col gap-1 absolute left-0 transition-all duration-200 delay-200 hover:w-[240px]"
+      className="bg-[#fff]  w-[75.5px] h-[calc(100vh-61px)] flex flex-col gap-1 absolute left-0 transition-all duration-200 delay-200 hover:w-[240px]"
       onMouseEnter={handleEnter}
       onMouseLeave={handleLeave}
     >
       <div
-        className="max-w-[56px] h-[56px] ml-[10px] rounded-[16px] bg-cyan-200 flex overflow-hidden transition-all duration-200 delay-200"
+        className="max-w-[56px] h-[56px] ml-[10px] rounded-[16px] bg-[#c2e7ff] flex overflow-hidden transition-all duration-200 delay-200"
         ref={ref1}
       >
         <div className="min-w-[56px] h-[56px]  rounded-[16px] flex items-center justify-center">
@@ -53,23 +53,23 @@ export default function Sidebar() {
         </div>
         <div className="min-w-[calc(133px-56px)] h-[56px] flex items-center text-sm">Compose</div>
       </div>
-      <div className="w-full h-[calc(100vh-61px-54px)] mt-1.5 bg-gray-200 flex flex-col overflow-hidden">
+      <div className="w-full h-[calc(100vh-61px-54px)] mt-1.5 flex flex-col overflow-hidden">
         {data.map((item, index) => {
           return (
             <Link
               to={`/${item.name}`}
               key={index}
               onClick={() => {
-                refArray.current[currentPage].classList.remove('bg-cyan-200');
+                refArray.current[currentPage].classList.remove('bg-[#D3E3FD]');
+                refArray.current[index].classList.add('bg-[#D3E3FD]');
                 setCurrentPage((prev) => (prev = index));
-                refArray.current[index].classList.add('bg-cyan-200');
               }}
               ref={(el) => (refArray.current[index] = el)}
               className={`max-w-[53px] overflow-hidden h-[32px]  rounded-r-full flex transition-all duration-200 delay-200`}
             >
               <div
                 className={`max-w-[32px] h-[32px] ml-5 rounded-full  flex ${
-                  index === currentPage ? 'bg-cyan-200' : ''
+                  index === currentPage ? 'bg-[#D3E3FD]' : ''
                 }`}
               >
                 <div className=" min-w-[32px] min-h-[32px] flex justify-center items-center">
